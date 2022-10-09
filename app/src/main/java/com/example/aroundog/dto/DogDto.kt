f4e4e3928c80e4c5d9f74e7be723d1575e326693
hyeonmin <hyeonmin97@naver.com>
2022-10-09 22:30:55 +0900

    [#12] update profile with DogFragment
    DogFragment를 사용하도록 변경

--- app/src/main/java/com/example/aroundog/dto/DogDto.kt
+++ app/src/main/java/com/example/aroundog/dto/DogDto.kt
@@ -2,6 +2,6 @@ package com.example.aroundog.dto
 
 import com.example.aroundog.Model.Gender
 
-data class DogDto(var dogId:Long, var dogName:String, var dogAge:Int, var dogWeight:Double, var dogHeight:Double, var dogGender: Gender, var breed:Long){
+data class DogDto(var dogId:Long, var dogName:String, var dogAge:Int, var dogWeight:Double, var dogHeight:Double, var dogGender: Gender, var breed:Long):java.io.Serializable{
 
 }
