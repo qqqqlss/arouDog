d23b843dc3c28d62139d113db75370aa0cf8d310
hyeonmin <hyeonmin97@naver.com>
2022-10-10 17:25:32 +0900

    [#12] get dog image from server
    로그인 시 서버에서 강아지 사진 불러오는 기능 추가

--- app/src/main/java/com/example/aroundog/Model/DogSliderAdapter.kt
+++ app/src/main/java/com/example/aroundog/Model/DogSliderAdapter.kt
@@ -1,21 +1,33 @@
 package com.example.aroundog.Model
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
+import android.util.Base64
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.aroundog.R
+import com.example.aroundog.dto.ImgDto
 import org.jetbrains.anko.find
 
-class DogSliderAdapter(val imgList:List<Int>): RecyclerView.Adapter<DogSliderAdapter.ViewHolder>() {
+class DogSliderAdapter(val imgList: List<ImgDto>): RecyclerView.Adapter<DogSliderAdapter.ViewHolder>() {
+    val TAG = "DOGSLIDERADAPTER"
 
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         var dogSlider:ImageView
+        var view = view
+        var path: String = ""
         init {
             dogSlider = view.findViewById(R.id.dogSlider)
+            dogSlider.setOnClickListener {
+                Toast.makeText(view.context, "${path}", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -24,7 +36,19 @@ class DogSliderAdapter(val imgList:List<Int>): RecyclerView.Adapter<DogSliderAda
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.dogSlider.setImageResource(imgList[position])
+        holder.path = imgList[position].path
+
+        var bitmap: Bitmap
+        if (imgList[position].path == "error") {//비어있는경우
+            holder.dogSlider.setImageResource(R.drawable.error2)
+        } else {
+            var byteArray: ByteArray = Base64.decode(imgList[position].img, Base64.DEFAULT)
+            bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+            Log.d(TAG, "itemList is not null")
+            Log.d(TAG, "${imgList}")
+            holder.dogSlider.setImageBitmap(bitmap)
+        }
+
     }
 
     override fun getItemCount(): Int {
